24776cdd81b31c7293529f45112464e344efeb3e
Wagner Araujo <araujowp@gmail.com>
2025-12-27 09:32:45 -0300

    no cards page add option add cards

--- lib/page/statistics/no_cards_page.dart
+++ lib/page/statistics/no_cards_page.dart
@@ -1,3 +1,4 @@
+import 'package:capcards/page/card/search_card_page.dart';
 import 'package:capcards/repository/deck/deck_dto.dart';
 import 'package:flutter/material.dart';
 
@@ -5,14 +6,43 @@ class NoCardsPage extends StatelessWidget {
   final DeckDTO deckDto;
 
   const NoCardsPage(this.deckDto, {super.key});
+
+  editCards(BuildContext context, int deckId) async {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => SearchCardPage(deckId: deckId)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(deckDto.description),
       ),
-      body: const Center(
-        child: Text('Nenhum cartão encontrado'),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Nenhum cartão encontrado',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            ElevatedButton.icon(
+              onPressed: () => editCards(context, deckDto.id),
+              icon: const Icon(Icons.add), // Ou Icons.edit se preferir
+              label: const Text('Adicionar cartões'),
+              style: ElevatedButton.styleFrom(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                textStyle: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
